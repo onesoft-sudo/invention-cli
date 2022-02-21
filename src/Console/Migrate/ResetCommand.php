c558d819e1da69fff91be3b77ce57c1cd477410e
Ar Rakin <rakinar2@gmail.com>
2022-02-21 20:45:21 +0600

    Bug fixes

--- src/Console/Migrate/ResetCommand.php
+++ src/Console/Migrate/ResetCommand.php
@@ -3,7 +3,7 @@
 
 namespace OSN\Invention\CLI\Console\Migrate;
 
-use OSN\Invention\Utils\Migrations;
+use OSN\Invention\CLI\Utils\Migrations;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
