827926130287237839d6ff32cbfee1e5c171bc61
Ar Rakin <rakinar2@gmail.com>
2022-03-25 22:50:56 +0600

    Added make:component command

---
+++ src/Console/Make/ComponentCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace OSN\Invention\CLI\Console\Make;
+
+
+use OSN\Framework\DataTypes\_String;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+class ComponentCommand extends Command
+{
+    protected static $defaultName = "make:component";
+    protected static $defaultDescription = "Generate a new view component class and view template";
+
+    protected function configure()
+    {
+        $this
+            ->setHelp("Generate a new view component class at {ROOT_DIR}/app/ViewComponents/ and a new view template at {ROOT_DIR}/resources/views/components/")
+            ->addArgument('name', InputArgument::REQUIRED, 'The view component name [like a normal PHP class name]');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $name = $input->getArgument('name');
+        $viewName = _String::from($name)->camel2slug()->__toString();
+
+        $this->generator->generate(app()->config["root_dir"] . "/app/ViewComponents/{$name}.php", 'component-renderer.php', function () use ($output, $name) {
+            $output->writeln("<info>Component class created</info>: $name");
+        }, function ($e) use ($name, $output) {
+            $output->writeln("<error>Cannot generate component class</error>: $name.php: <comment>$e</comment>");
+            exit(-1);
+        }, $name, $viewName);
+
+        $this->generator->generate(app()->config["root_dir"] . "/resources/views/components/{$viewName}.power.php", 'component-view.power.php', function () use ($viewName, $output) {
+            $output->writeln("<info>Component view template created</info>: $viewName");
+        }, function ($e) use ($viewName, $output) {
+            $output->writeln("<error>Cannot generate component view template</error>: $viewName.php: <comment>$e</comment>");
+            exit(-1);
+        });
+
+        return 0;
+    }
+}
